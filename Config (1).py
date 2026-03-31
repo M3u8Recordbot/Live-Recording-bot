7a1ccf9e77eb0428de64c394fd4e8bf22dba0c48
M3u8Recordbot <littlesinghamhy@gmail.com>
2026-03-31 14:41:09 +0530

    Update bot token and username in Config
    Updated bot configuration with new token and username.

--- Config (1).py
+++ Config (1).py
@@ -2,7 +2,7 @@
 import os
 
 # Bot token from @botfather
-BOT_TOKEN = ""
+BOT_TOKEN = "8619959255:AAHq2ekj90Mwx2cI6IdNd-b16JC86YTDMlM"
 # From my.telegram.org/
 API_ID = "29481626"
 API_HASH =  "4892185769903521077c4cea97808b8c"
@@ -12,6 +12,6 @@ OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "969084369").split(" ")]
 AUTH_USERS = [969084369, 2800583882]
 # Time gap after each request (in seconds) 
 TIME_GAP = 0
-BOT_USERNAME='#' #bot username without@
-API_TOKEN=''#'t change this without developer permission
+BOT_USERNAME='M3u8LiveRecordingBot' #bot username without@
+API_TOKEN='9503d9bf87c90aa9e0aab35d4dec7d1ce24c0a23't change this without developer permission
 
