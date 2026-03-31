0a86c315e37f172ca25edf91a0a74657da788cb4
M3u8Recordbot <littlesinghamhy@gmail.com>
2026-03-31 14:39:27 +0530

    Update API credentials in Config (1).py
    Updated API_ID and API_HASH values in the configuration.

--- Config (1).py
+++ Config (1).py
@@ -1,12 +1,11 @@
-#KSBOTOFFICALS
-#DEVELOPER
+
 import os
 
 # Bot token from @botfather
 BOT_TOKEN = ""
 # From my.telegram.org/
-API_ID = ""
-API_HASH =  ""
+API_ID = "29481626"
+API_HASH =  "4892185769903521077c4cea97808b8c"
 # For /log cmd
 OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "969084369").split(" ")]
 # No time limit for this users
@@ -14,5 +13,5 @@ AUTH_USERS = [969084369, 2800583882]
 # Time gap after each request (in seconds) 
 TIME_GAP = 0
 BOT_USERNAME='#' #bot username without@
-API_TOKEN='' #don't change this without developer permission
+API_TOKEN=''#'t change this without developer permission
 
